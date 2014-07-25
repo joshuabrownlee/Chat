5016211afbbbf97ab3efc1e4c4e93dddc3d612c3
joshuabrownlee <brownlee.joshua@yahoo.com>
2014-07-25 11:45:23 -0500

    Test

--- Chat.web/App/Scripts/HomeController.js
+++ Chat.web/App/Scripts/HomeController.js
@@ -1,13 +1,13 @@
-﻿app.controller('homeController', function ($scope, $http, $q, LoginService, $window, $location) {
-    $scope.loggedIn = false;
-    $scope.token = 'Please Log In'
-    $scope.login = function () {
-        LoginService.processLogin($scope.user.username, $scope.user.password).then(function () {
-            $scope.token = "Logged In Successfully"; document.getElementById("text").value = "";
-            document.getElementById("password").value = "";
-            $location.path('/userView');
-        }, function (status) {
-            $scope.token = status;
-        });
-    };
-})
+﻿//app.controller('homeController', function ($scope, $http, $q, LoginService, $window, $location) {
+//    $scope.loggedIn = false;
+//    $scope.token = 'Please Log In'
+//    $scope.login = function () {
+//        LoginService.processLogin($scope.user.username, $scope.user.password).then(function () {
+//            $scope.token = "Logged In Successfully"; document.getElementById("text").value = "";
+//            document.getElementById("password").value = "";
+//            $location.path('/userView');
+//        }, function (status) {
+//            $scope.token = status;
+//        });
+//    };
+//})
